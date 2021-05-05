79c67a29692990358db1d9c44b303c3911d359fe
musleh0001 <khan.muslehuddin0001@gmail.com>
2021-05-06 03:52:20 +0600

    :white_check_mark: adds unitest
    adds unitest for company app

--- api/coronavstech/companies/urls.py
+++ api/coronavstech/companies/urls.py
@@ -4,4 +4,4 @@ from rest_framework import routers
 from .views import CompanyViewSet
 
 companies_router = routers.DefaultRouter()
-companies_router.register('Companies', viewset=CompanyViewSet, basename='companies')
+companies_router.register('companies', viewset=CompanyViewSet, basename='companies')
